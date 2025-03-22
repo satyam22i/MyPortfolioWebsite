a019ba272814924650a65e9849a9bbb574918ebc
Satyam Srivastav <162419614+satyam22i@users.noreply.github.com>
2025-03-22 18:23:42 +0530

    Add files via upload

---
+++ frontend/src/components/skill.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import "./MySkills.css";
+
+const MySkills = () => {
+  return (
+    <section className="skills-container">
+      <div className="skills-content">
+        <h2>My Skills</h2>
+        <p>
+          I specialize in web development using the MERN stack and have
+          expertise in data analytics. Here are some of the technologies I work
+          with:
+        </p>
+        <ul className="skills-list">
+          <li>React.js</li>
+          <li>Node.js</li>
+          <li>Express.js</li>
+          <li>MongoDB</li>
+          <li>SQL</li>
+          <li>PostgreSQL</li>
+          <li>JavaScript</li>
+          <li>CSS</li>
+          <li>HTML</li>
+          <li>Python</li>
+          <li>Power BI</li>
+          <li>Pandas</li>
+          <li>Excel</li>
+        </ul>
+      </div>
+    </section>
+  );
+};
+
+export default MySkills;
